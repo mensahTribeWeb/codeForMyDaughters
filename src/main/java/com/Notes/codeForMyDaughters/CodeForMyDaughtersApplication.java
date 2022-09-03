3019cc34dae9cf7ab6f1b8d1ee38a7abf584f327
mensahTribeWeb <ndm.acct85@gmailcom>
2022-09-02 19:13:13 -0500

    first commit

---
+++ src/main/java/com/Notes/codeForMyDaughters/CodeForMyDaughtersApplication.java
@@ -0,0 +1,13 @@
+package com.Notes.codeForMyDaughters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeForMyDaughtersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeForMyDaughtersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Notes/codeForMyDaughters/CodeForMyDaughtersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Notes.codeForMyDaughters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeForMyDaughtersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
